afdad672c5a6f0e8ed14e97b1c8b4728c32ed51f
MichaelBoyd <9999.michael@gmail.com>
2016-02-19 01:04:40 -0600

    Add setters so Add/Edit form would accept input

--- pablo-singing-monster/src/main/java/com/monster/domain/Monster.java
+++ pablo-singing-monster/src/main/java/com/monster/domain/Monster.java
@@ -58,11 +58,24 @@ public class Monster {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getDescription() {
 		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Island getIsland() {
 		return island;
 	}
+
+	public void setIsland(Island island) {
+		this.island = island;
+	}
+
 }
