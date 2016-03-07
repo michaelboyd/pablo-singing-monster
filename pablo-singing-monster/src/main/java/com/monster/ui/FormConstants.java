4d19ea208ec738449707b4851f6cdc9d9e04a420
Michael Boyd <michael.boyd@dps.texas.gov>
2016-03-07 15:48:10 -0600

    Add a forward slash to end of folder paths

--- pablo-singing-monster/src/main/java/com/monster/ui/FormConstants.java
+++ pablo-singing-monster/src/main/java/com/monster/ui/FormConstants.java
@@ -5,7 +5,7 @@ public interface FormConstants {
 	public static final String CANCELED_NOTIFICATION_LABEL = "Canceled";
 	public static final String DELETED_NOTIFICATION_LABEL = "Deleted";
 	public static final String SAVED_NOTIFICATION_LABEL = "Saved";
-	public static final String UPLOAD_FOLDER_IMAGE = "uploads/image";
-	public static final String UPLOAD_FOLDER_AUDIO = "uploads/audio";
+	public static final String UPLOAD_FOLDER_IMAGE = "uploads/image/";
+	public static final String UPLOAD_FOLDER_AUDIO = "uploads/audio/";
 	
 }
